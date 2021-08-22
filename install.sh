5aa5d3bbb3e9bdbfe2a65f0951ad2eaf54010d05
alirezabashi98 <alirezabashi98@gmail.com>
2021-08-22 19:00:08 +0430

    End of trial version - Added general folder

--- install.sh
+++ install.sh
@@ -16,23 +16,23 @@ function main() {
         echo "install all package"
         run_script "package/allPackage.sh" "allPackage.sh"
         ;;
-    2)
-        echo -n "run adobe camera Raw installer"
-        run_script "scripts/cameraRawInstaller.sh" "cameraRawInstaller.sh"
-        ;;
-    3)
-        echo "run winecfg..."
-        echo -n "open virtualdrive configuration..."
-        run_script "scripts/winecfg.sh" "winecfg.sh"
-        ;;
-    4)
-        echo -n "uninstall photoshop CC ..."
-        run_script "scripts/uninstaller.sh" "uninstaller.sh"
-        ;;
-    5)
-        echo "exit setup..."
-        exitScript
-        ;;
+#    2)
+#        echo -n "run adobe camera Raw installer"
+#        run_script "scripts/cameraRawInstaller.sh" "cameraRawInstaller.sh"
+#        ;;
+#    3)
+#        echo "run winecfg..."
+#        echo -n "open virtualdrive configuration..."
+#        run_script "scripts/winecfg.sh" "winecfg.sh"
+#        ;;
+#    4)
+#        echo -n "uninstall photoshop CC ..."
+#        run_script "scripts/uninstaller.sh" "uninstaller.sh"
+#        ;;
+#    5)
+#        echo "exit setup..."
+#        exitScript
+#        ;;
     esac
 
 }
